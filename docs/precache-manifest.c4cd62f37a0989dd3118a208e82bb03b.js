76059a27bddc3a7144d17bc7d56b8affb2285d56
kotomi <jh223sk@gmail.com>
2021-07-27 16:20:28 +0900

    docsフォルダへデプロイ先を変更。およびビルドした。

---
+++ docs/precache-manifest.c4cd62f37a0989dd3118a208e82bb03b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4c1b52a407ac2238aaf",
+    "url": "css/app.6736d48b.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "61bfcf4ead451456dcfb86e92ac5c68b",
+    "url": "index.html"
+  },
+  {
+    "revision": "64fa41dddb215016df37",
+    "url": "js/about.1f82111f.js"
+  },
+  {
+    "revision": "c4c1b52a407ac2238aaf",
+    "url": "js/app.2e2dd5b7.js"
+  },
+  {
+    "revision": "1ea1ab00e63eead3f516",
+    "url": "js/chunk-vendors.d5fcdf58.js"
+  },
+  {
+    "revision": "0b91c8cfb9612f47020c21aa1bfe37ff",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
